b10b4ad910750d0ca69887fb8376dee70b2cc1f1
Abhay Panchal <abpanchal951357@gmail.com>
2024-04-07 17:26:29 +0530

    chore: map and for loop

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
 const Card = () => {
   return (
     <div className="card">
-      <h1 className="card-title">This is a card title</h1>
+      <h1 className="card-title text-primary">This is a card title</h1>
       <p className="card-body">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius minima
         velit deleniti. Corporis, voluptate in. Iure praesentium at sapiente
